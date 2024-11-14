dcc0b6c166dd70158b9d1cdfcd150ca61bbc1f05
fauzirahmat89 <fauzirahmat89@gmail.com>
2024-11-14 21:26:32 +0700

    Add product management endpoints

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,18 @@ const port = 3000;
 
 app.use(express.json());
 
+// Endpoint untuk menambah produk
+app.post('/products', (req: Request, res: Response) => {
+    const { name, price } = req.body;
+    // Logika untuk menambah produk
+    res.send('Product added');
+});
 
+// Endpoint untuk mendapatkan semua produk
+app.get('/products', (req: Request, res: Response) => {
+    // Logika untuk mendapatkan semua produk
+    res.send('List of products');
+});
 
 
 
