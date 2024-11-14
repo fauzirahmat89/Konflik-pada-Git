91614240ad3f31bbd2e2305ee979946d9ee3c257
fauzirahmat89 <fauzirahmat89@gmail.com>
2024-11-14 21:19:36 +0700

    sesuaikan soal

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,7 @@ const port = 3000;
 app.use(express.json());
 
 
-// Endpoint untuk register user
-app.post('/register', (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    // Logika untuk register user
-    res.send('User registered');
-});
 
-// Endpoint untuk login user
-app.post('/login', (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    // Logika untuk login user
-    res.send('User logged in');
-});
 
 
 
